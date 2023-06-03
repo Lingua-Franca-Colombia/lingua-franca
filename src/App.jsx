82528b9aad59b78ece8f5bc19955abbe5b601529
jalejotorresm <alejandrotorres5286@gmail.com>
2023-06-02 21:16:35 -0500

    Landing Page Lingua Franca v1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import MainPage from "./components/MainPage";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage/index.jsx
@@ -0,0 +1,14 @@
+import React, { useRef } from "react";
+import Hero from "../Hero";
+import Features from "../Features";
+
+function MainPage() {
+  return (
+    <>
+      <Hero />
+      <Features />
+    </>
+  );
+}
+
+export default MainPage;
