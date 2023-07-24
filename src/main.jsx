23fc78c194aa19bcc1d146413673fdae88c5a7b6
Alejandro Torres <72758522+jalejotorresm@users.noreply.github.com>
2023-07-24 13:51:40 +0000

    fonts updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import * as React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+
+const theme = extendTheme({
+  colors:{
+    brand:{
+      100: "#5e17eb",
+      200: "#1bd527"
+    }
+  }
+})
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
